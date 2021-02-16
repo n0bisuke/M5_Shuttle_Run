654bacfa02ebc28c0c76aab76c519145a7cdf2ba
n0bisuke <rsksound@gmail.com>
2021-02-16 11:44:34 +0900

    test

--- _M5SR_Melody.cpp
+++ _M5SR_Melody.cpp
@@ -2,7 +2,6 @@
 #include "_M5SR_Melody.h"
 
 #define NOTE_DO_L 130.813 //ドL
-#define NOTE_SOL_L 195.998 //ソL
 #define NOTE_DO 261.626 //ド
 #define NOTE_RE 293.665 //レ
 #define NOTE_MI 329.628 //ミ
@@ -12,7 +11,6 @@
 #define NOTE_TI 493.883 //シ
 #define NOTE_DO_H 523.251 //ドH
 #define NOTE_RE_H 587.330 //レH
-#define NOTE_MI_H 659.255 //ミH
 
 //行き
 void SR_Melody_Going(int duration, float delayTime){  
@@ -42,14 +40,14 @@ void SR_Melody_Returning(int duration, float delayTime){
 
 //レベルアップ音サウンドエフェクト
 void SR_SE_LevelUp(){
-   M5.Speaker.tone(NOTE_DO_H, 10); delay(250);
+   M5.Speaker.tone(NOTE_DO_L, 10); delay(250);
    M5.Speaker.tone(NOTE_SOL, 10); delay(250);
-   M5.Speaker.tone(NOTE_RE_H, 10); delay(250);
+   M5.Speaker.tone(NOTE_RE_H, 10); delay(500);
 }
 
 //スタート時サウンドエフェクト
 void SR_SE_Start(){
-   M5.Speaker.tone(NOTE_DO, 10); delay(250);
-   M5.Speaker.tone(NOTE_SOL_L, 10); delay(250);
-   M5.Speaker.tone(NOTE_RE, 10); delay(500);
+   M5.Speaker.tone(NOTE_DO_L, 10); delay(250);
+   M5.Speaker.tone(NOTE_SOL, 10); delay(250);
+   M5.Speaker.tone(NOTE_RE_H, 10); delay(500);
 }
